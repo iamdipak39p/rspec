fb666cfdef73d52d5aefadbff818217485074efb
Dipak Patil <iamdipak39@gmail.com>
2025-05-14 18:16:49 +0530

    basic test cases

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,15 +1,21 @@
 class Card
-  attr_reader :type
+  attr_reader :type, :rank
 
-  def initialize(type)
+  def initialize(rank,type)
     @type = type
+    @rank = rank
   end
 end
 
 RSpec.describe "Card" do
   it 'has type' do
-    card = Card.new('Ace of Spades')
-    expect(card.type).to eq('Ace of Spades')
+    card = Card.new('Ace', 'Spades')
+    expect(card.type).to eq('Spades')
+  end
+
+  it 'has rank' do
+    card = Card.new('Ace', 'Spades')
+    expect(card.rank).to eq('Ace')
   end
 end
 
